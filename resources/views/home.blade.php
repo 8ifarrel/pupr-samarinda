c23228e01b124e9595f89fbb6b1ad94f358c8b9b
Farrel Sirah <farrelsirah@gmail.com>
2024-01-03 23:32:17 +0800

    baru masuk ke bagian struktur organisasi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,12 +23,14 @@
 </div>
 
 {{-- Sambutan Kepala Dinas --}}
-<div class="d-flex flex-row mt-5 mb-5">
+<div class="d-flex flex-row my-5">
   {{-- Foto Kepala Dinas --}}
-  <img src="/assets/temp/desy-damayanti-st-mt.png" class="img-fluid ms-5" alt="">
+  <div class=" ms-5 px-5">
+    <img src="/assets/temp/desy-damayanti-st-mt.png" class="img-fluid" alt="">
+  </div>
 
   {{-- Teks Kepala Dinas --}}
-  <div class="ms-auto" style="background-color: #F8AC00;">
+  <div class="card border-0 rounded-0 ms-auto w-75" style="background-color: #F8AC00;">
     <div class="m-5">
       <p class="m-0 p-0 mb-3 fs-5 fw-bold">
         SAMBUTAN KEPALA DINAS
@@ -38,7 +40,7 @@
         DESY DAMAYANTI S.T., M.T.
       </p>
 
-      <p class=" fs-5 m-0 p-0 mb-4 fw-bold" style="color: rgba(0, 0, 0, 0.55 )">
+      <p class=" fs-5 m-0 p-0 mb-4 fw-bold text-black-50">
         Kepala Dinas Pekerjaan Umum dan Penataan Ruang Kota Samarinda
       </p>
 
@@ -49,48 +51,63 @@
   </div>
 </div>
 
-{{-- Berita PUPR Samarinda --}}
+{{-- Berita Terbaru PUPR Samarinda --}}
 <div class="container">
-  <div class="row">
-    
-    <?php for ($i = 0; $i < 6; $i++) { ?>
-      <div class="col-md-4 mb-3">
+  <h1 class="mb-4 text-center">
+    Berita Terkini DPUPR Kota Samarinda
+  </h1>
+
+  <div class="row" style="margin-left: 5rem; margin-right: 5rem;">
+    <?php for ($i = 0; $i < 4; $i++) { ?>
+      <div class="col-md-3 mb-3">
         <div class="card h-100">
-          <a href="">
-            <div class="position-absolute m-2 bg-dark py-2 px-3 text-white border rounded" style="--bs-bg-opacity: .8;">
-              Ini kategori pos
-            </div>
-          </a>
+          {{-- Foto berita --}}
+          <a href=""><img src="https://source.unsplash.com/300x300?random" class="card-img-top" alt=""></a>
           
-          <img src="https://source.unsplash.com/300x300?random"  class="card-img-top" alt="">
-  
           <div class="card-body d-flex align-items-start flex-column">
-            <h5 class="card-title">
-              Ini judul
+            {{-- Judul berita --}}
+            <h5 class="card-title fw-bold">
+              <a href="" class="text-decoration-none text-black">
+                Ini Judul
+              </a>
             </h5>
   
-            <p>
+            {{-- Dari bidang apa --}}
+            <p class="mb-0 pb-0">
               <small> 
-                By 
-                <a href="" class="text-decoration-none">
-                  By Farrel
+                <a href="" class="text-decoration-none fw-bold text-black-50">
+                  UPTD Saluran Drainase dan Sungai
                 </a> 
-                16 jam lalu.
               </small>
             </p>
-  
-            <p class="card-text">
-              Ini excerpt. Ini excerpt. Ini excerpt. Ini excerpt. Ini excerpt. Ini excerpt. Ini excerpt. Ini excerpt.
+
+            <p class="mt-0 pt-0">
+              <small>
+                16 jam lalu
+              </small>
             </p>
-  
-            <a href="" class="btn btn-primary mt-auto">
-              Read more
+            
+            {{-- Tombol baca selengkapnya --}}
+            <a href="" class="mt-auto text-decoration-none">
+              Klik untuk baca selengkapnya
             </a>
           </div>
         </div>
       </div>
-      <?php } ?>
+    <?php } ?>
+
+    {{-- Tombol lihat semua berita --}}
+    <div class="d-flex justify-content-center">
+      <a href="" class="btn btn-primary fw-bold rounded-5 border-0 py-2 px-5" style="background-color: #16156C;"> 
+        Lihat semua berita
+      </a>
+    </div>
   </div>
+</div>
+
+{{-- Struktur Organisasi (Bidang-bidang Kami) --}}
+<div class="" style="background-color: #F8AC00;">
+
 </div>
     
 @endsection
