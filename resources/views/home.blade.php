794af22bbd31ebfdefbba246f41b62623b7eaebf
Farrel Sirah <farrelsirah@gmail.com>
2024-01-03 08:38:33 +0800

    navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,17 @@
-test
+@extends('layouts.main')
+
+@section('container')
+
+{{-- Gambar Slide --}}
+<div class="img-fluid" style="border: 0;">
+  <img src="/assets/temp/slide.png" class="card-img rounded-0" alt="...">
+</div>
+
+{{-- Sambutan Kepala Dinas --}}
+<div class="row">
+  <div class="col-lg-4 p-5 w-100" style="background-color: #F8AC00">
+    <img src="" class="rounded float-start" alt="">
+  </div>
+</div>
+    
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,98 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #16156C !important;">
+  <div class="container">
+
+    {{-- Logo PUPR Kota Samarinda --}}
+    <a class="navbar-brand text-white" href="#">LOGO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    {{-- Isi navbar --}}
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav ms-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+        
+        {{-- Home --}}
+        <li class="nav-item text-white">
+          <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+        </li>
+        
+        {{-- Profil --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profil
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Profil Kepala</a></li>
+            <li><a class="dropdown-item" href="#">Riwayat Pimpinan</a></li>
+            <li><a class="dropdown-item" href="#">Sejarah Kota Samarinda</a></li>
+            <li><a class="dropdown-item" href="#">Sejarah PUPR Kota Samarinda</a></li>
+            <li><a class="dropdown-item" href="#">Struktur Organisasi</a></li>
+            <li><a class="dropdown-item" href="#">Tugas Pokok dan Fungsi</a></li>
+            <li><a class="dropdown-item" href="#">Visi dan Misi</a></li>
+          </ul>
+        </li>
+
+        {{-- Informasi Dinas --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Informasi Dinas
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Pengumuman</a></li>
+            <li><a class="dropdown-item" href="#">Berita</a></li>
+          </ul>
+        </li>
+        
+        {{-- Bidang Teknis --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Bidang Teknis
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Sekretariat</a></li>
+            <li><a class="dropdown-item" href="#">Bidang Bina Konstruksi</a></li>
+            <li><a class="dropdown-item" href="#">Bidang Bina Marga</a></li>
+            <li><a class="dropdown-item" href="#">Bidang Cipta Karya</a></li>
+            <li><a class="dropdown-item" href="#">Bidang Pertahanan</a></li>
+            <li><a class="dropdown-item" href="#">Bidang Sumber Daya Air</a></li>
+            <li><a class="dropdown-item" href="#">Bidang Tata Ruang</a></li>
+            <li><a class="dropdown-item" href="#">UPTD Pemeliharaan Jalan dan Jembatan</a></li>
+            <li><a class="dropdown-item" href="#">UPTD Pemeliharaan Saluran Drainase dan Irigasi</a></li>
+            <li><a class="dropdown-item" href="#">UPTD Pengelolaan Air Limbah Domestik</a></li>
+          </ul>
+        </li>
+
+        {{-- PPID Pelaksana --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PPID Pelaksana
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Audit Laporan Keuangan</a></li>
+            <li><a class="dropdown-item" href="#">Pengadaan Barang dan Jasa</a></li>
+            <li><a class="dropdown-item" href="#">Informasi yang Wajib Disediakan dan Diumumkan Secara Berkala</a></li>
+            <li><a class="dropdown-item" href="#">Informasi yang Wajib Diumumkan Secara Serta-merta</a></li>
+            <li><a class="dropdown-item" href="#">Informasi yang Wajib Tersedia Setiap Saat</a></li>
+            <li><a class="dropdown-item" href="#">Laporan Kinerja 2022</a></li>
+            <li><a class="dropdown-item" href="#">Laporan Operasional 2022</a></li>
+            <li><a class="dropdown-item" href="#">Laporan Perubahan Ekuitas 2022</a></li>
+            <li><a class="dropdown-item" href="#">Laporan Realisasi Anggaran Pendapatan dan Belanja Daerah 2022</a></li>
+          </ul>
+        </li>
+
+        {{-- E-Library --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            E-Library
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Galeri</a></li>
+            <li><a class="dropdown-item" href="#">Video</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+    
+  </div>
+</nav>
