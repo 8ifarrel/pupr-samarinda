3a4364d6522b20df634b2dedc24091956dc0ecc2
louvyo <miftahulh654@gmail.com>
2024-01-06 18:59:32 +0800

    new date js

--- public/js/time.js
+++ public/js/time.js
@@ -1,34 +1,3 @@
-// function updateClock() {
-//     var currentTime = new Date();
-
-//     var optionsDate = {
-//         timeZone: 'Asia/Makassar',
-//         weekday: 'long',
-//         year: 'numeric',
-//         month: 'long',
-//         day: 'numeric'
-//     };
-    
-//     var optionsTime = {
-//         timeZone: 'Asia/Makassar',
-//         hour12: false, 
-//         hour: '2-digit',
-//         minute: '2-digit',
-//         second: '2-digit',
-//         timeZoneName: 'short'
-//     };
-    
-//     var formattedDate = currentTime.toLocaleDateString('id-ID', optionsDate);
-//     var formattedTime = currentTime.toLocaleTimeString('id-ID', optionsTime);
-    
-//     document.getElementById('current-time').innerHTML = formattedDate + ' | ' + formattedTime;
-
-//     requestAnimationFrame(updateClock);
-// }
-
-// // Update the clock every second
-// updateClock();
-
 function updateClock() {
     var elementJam = document.getElementById("current-time");
     var now = new Date();
